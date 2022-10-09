15b75326e3a803e355ad66ad6f11493e5494e126
Carlos <josesolrac13@gmail.com>
2022-10-09 18:43:16 -0500

    estilos en Card

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,28 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./styles/userCard.css"
 
-const UserCard = ({user, deleteUser,setupdateInfo}) => {
-
-
+const UserCard = ({ user, deleteUser, setupdateInfo }) => {
   const handleEdit = () => {
-    setupdateInfo(user)
-
-  }
-
-  
+    setupdateInfo(user);
+  };
 
   return (
-    <article>
-      <h2>{`${user.first_name} ${user.last_name}`}</h2>
-      <ul className="card">
-        <li className="card_item"><span>Email: </span>{user.email}</li>
-        <li className="card_item"><span>Birthday: </span>{user.birthday}</li>
+    <article className="card">
+      <h2 className="card__name">{`${user.first_name} ${user.last_name}`}</h2>
+      <ul className="card__list">
+        <li className="card__item">
+          <span className="card__span">Email: </span>
+          {user.email}
+        </li>
+        <li className="card__item">
+          <span className="card__span">Birthday:</span>
+          <div className="card__item-container">
+          <i className="card_gift fa-solid fa-gift"></i> {user.birthday}-
+          </div>
+        </li>
       </ul>
-      <footer>
-      <i onClick={() => deleteUser(user.id)} className="fa-solid fa-trash-can"></i>
-      <i onClick={handleEdit} className="fa-solid fa-user-pen"></i>
+      <footer className="card__footer">
+        <button className="card__btn" onClick={() => deleteUser(user.id)}>
+        <i className="card__tash fa-solid fa-trash-can"></i>
+        </button>
+        <button className="card__btn" onClick={handleEdit}>
+        <i className="card__edit fa-solid fa-user-pen"></i>
+        </button>
       </footer>
     </article>
-  )
-}
+  );
+};
 
-export default UserCard
+export default UserCard;
